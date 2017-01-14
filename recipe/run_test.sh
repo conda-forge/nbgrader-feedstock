4402c3f4e53f406de77c28cfbe44d04f19063360
Logan Page <page.lg@gmail.com>
2017-01-14 11:08:11 +0200

    add formgrade and nbextension tests to run_tests.sh

--- recipe/run_test.sh
+++ recipe/run_test.sh
@@ -1,4 +1,6 @@
-# "${PREFIX}/bin/npm" install phantomjs-prebuilt
+npm install -g phantomjs-prebuilt
 
 # Call py.test directly as invoke causes CircleCI to bomb out
 py.test -v -x -m "not formgrader and not nbextensions"
+py.test -v -x -m "formgrader"
+py.test -v -x -m "nbextensions"
