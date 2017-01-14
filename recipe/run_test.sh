08bb2ff0ce2eb34b9f3fb7cd1c16c70f260acf6f
Logan Page <page.lg@gmail.com>
2017-01-14 11:17:19 +0200

    add phantomjs to path

--- recipe/run_test.sh
+++ recipe/run_test.sh
@@ -1,6 +1,7 @@
 npm install -g phantomjs-prebuilt
+export PATH=$PWD/node_modules/phantomjs-prebuilt/bin:$PATH
 
-# Call py.test directly as invoke causes CircleCI to bomb out
+# Call py.test directly as invoke causes CircleCI to bomb out, see #6
 py.test -v -x -m "not formgrader and not nbextensions"
 py.test -v -x -m "formgrader"
 py.test -v -x -m "nbextensions"
