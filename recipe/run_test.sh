afff7250a93c97662ae3116a3168feeea3e143fe
Logan Page <page.lg@gmail.com>
2017-01-14 13:28:23 +0200

    remove global install flag

--- recipe/run_test.sh
+++ recipe/run_test.sh
@@ -1,4 +1,4 @@
-npm install -g phantomjs-prebuilt
+npm install phantomjs-prebuilt
 export PATH=$PWD/node_modules/phantomjs-prebuilt/bin:$PATH
 
 # Call py.test directly as invoke causes CircleCI to bomb out, see #6
