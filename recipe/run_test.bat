b32821dde28656a4f330f793effda849ef15bba6
Jessica B. Hamrick <jhamrick@berkeley.edu>
2017-02-08 12:43:16 -0800

    Revert "Rerun failures"
    This reverts commit 3257306b15b5fe7ee434c3f49173eded23591a4b.

--- recipe/run_test.bat
+++ recipe/run_test.bat
@@ -3,5 +3,11 @@ IF %ERRORLEVEL% NEQ 0 EXIT /B %ERRORLEVEL%
 
 SET "PATH=%CD%\\node_modules\\phantomjs-prebuilt\\lib\\phantom\\bin;%PATH%"
 
-CALL py.test -v -x --rerun=4 || EXIT /B 1
+CALL py.test -v -x -m "not formgrader and not nbextensions" || EXIT /B 1
+IF %ERRORLEVEL% NEQ 0 EXIT /B %ERRORLEVEL%
+
+CALL py.test -v -x -m "formgrader" || EXIT /B 1
+IF %ERRORLEVEL% NEQ 0 EXIT /B %ERRORLEVEL%
+
+CALL py.test -v -x -m "nbextensions" || EXIT /B 1
 IF %ERRORLEVEL% NEQ 0 EXIT /B %ERRORLEVEL%
--- recipe/run_test.sh
+++ recipe/run_test.sh
@@ -2,4 +2,6 @@ npm install phantomjs-prebuilt
 export PATH=$PWD/node_modules/phantomjs-prebuilt/bin:$PATH
 
 # Call py.test directly as invoke causes CircleCI to bomb out, see #6
-py.test -v -x --rerun=4
+py.test -v -x -m "not formgrader and not nbextensions"
+py.test -v -x -m "formgrader"
+py.test -v -x -m "nbextensions"
