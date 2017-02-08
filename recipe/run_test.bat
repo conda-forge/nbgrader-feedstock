23c0aea6c2134d8b16ceecff6ae1bacc7b967d37
Jessica B. Hamrick <jhamrick@berkeley.edu>
2017-02-08 12:43:59 -0800

    Remove nbextension tests for now

--- recipe/run_test.bat
+++ recipe/run_test.bat
@@ -8,6 +8,3 @@ IF %ERRORLEVEL% NEQ 0 EXIT /B %ERRORLEVEL%
 
 CALL py.test -v -x -m "formgrader" || EXIT /B 1
 IF %ERRORLEVEL% NEQ 0 EXIT /B %ERRORLEVEL%
-
-CALL py.test -v -x -m "nbextensions" || EXIT /B 1
-IF %ERRORLEVEL% NEQ 0 EXIT /B %ERRORLEVEL%
