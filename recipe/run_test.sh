0529f1c454bfe368c6161cd0537b6cebe489ec80
Jessica B. Hamrick <jhamrick@berkeley.edu>
2017-07-08 13:24:03 -0700

    Make sure USER environment variable is set

--- recipe/run_test.sh
+++ recipe/run_test.sh
@@ -1,3 +1,6 @@
+# Make suer USER is set
+export USER=`whoami`
+
 # Call py.test directly as invoke causes CircleCI to bomb out, see #6
 py.test -v -x -m "not formgrader and not nbextensions"
 py.test -v -x -m "formgrader"
