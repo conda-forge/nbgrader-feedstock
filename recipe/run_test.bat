22fef632a46eb5b08626f0e72dc3a175f41d8b94
Jessica B. Hamrick <jhamrick@berkeley.edu>
2017-07-08 14:00:47 -0700

    Formgrader is no longer a separate test group

--- recipe/run_test.bat
+++ recipe/run_test.bat
@@ -1,5 +1,5 @@
-CALL py.test -v -x -m "not formgrader and not nbextensions" || EXIT /B 1
+CALL py.test -v -x -m "not nbextensions" || EXIT /B 1
 IF %ERRORLEVEL% NEQ 0 EXIT /B %ERRORLEVEL%
 
-CALL py.test -v -x -m "formgrader" || EXIT /B 1
-IF %ERRORLEVEL% NEQ 0 EXIT /B %ERRORLEVEL%
+rem CALL py.test -v -x -m "nbextensions" || EXIT /B 1
+rem IF %ERRORLEVEL% NEQ 0 EXIT /B %ERRORLEVEL%
--- recipe/run_test.sh
+++ recipe/run_test.sh
@@ -2,5 +2,5 @@
 export USER=`whoami`
 
 # Call py.test directly as invoke causes CircleCI to bomb out, see #6
-py.test -v -x -m "not formgrader and not nbextensions"
-py.test -v -x -m "formgrader"
+py.test -v -x -m "not nbextensions"
+# py.test -v -x -m "nbextensions"
