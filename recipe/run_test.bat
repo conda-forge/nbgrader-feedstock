33ac6ef7bef0df04f2114a60d1dc4725da0211b8
Jessica B. Hamrick <jhamrick@berkeley.edu>
2017-07-08 13:00:09 -0700

    Depend on phantomjs feedstock

--- recipe/run_test.bat
+++ recipe/run_test.bat
@@ -1,8 +1,3 @@
-CALL npm install phantomjs-prebuilt || EXIT /B 1
-IF %ERRORLEVEL% NEQ 0 EXIT /B %ERRORLEVEL%
-
-SET "PATH=%CD%\\node_modules\\phantomjs-prebuilt\\lib\\phantom\\bin;%PATH%"
-
 CALL py.test -v -x -m "not formgrader and not nbextensions" || EXIT /B 1
 IF %ERRORLEVEL% NEQ 0 EXIT /B %ERRORLEVEL%
 
--- recipe/run_test.sh
+++ recipe/run_test.sh
@@ -1,6 +1,3 @@
-npm install phantomjs-prebuilt
-export PATH=$PWD/node_modules/phantomjs-prebuilt/bin:$PATH
-
 # Call py.test directly as invoke causes CircleCI to bomb out, see #6
 py.test -v -x -m "not formgrader and not nbextensions"
 py.test -v -x -m "formgrader"
